25b97984f4f78da99b73313a3c458c88bdc65f68
Shyam Raghavan <shyam4apple13@gmail.com>
2019-01-02 21:51:34 +0530

    Jan 2

--- Motoserve/Classes/ConstraintsView/constraintViewController.m
+++ Motoserve/Classes/ConstraintsView/constraintViewController.m
@@ -18,9 +18,10 @@
     NSArray * bannerArray;
     NSInteger jslider;
     float xslider;
-    UIScrollView * bannerSrl;
+    UIScrollView * bannerSrl,*onrdScrl,*prebookScrl;
     NSTimer *timer;
     UIPageControl * pageControl;
+    UIButton *  prebkBtn,* onrdBtn;
 }
 @end
 
@@ -110,7 +111,7 @@
     tracklbl.translatesAutoresizingMaskIntoConstraints = NO;
     [tracklbl.topAnchor constraintEqualToAnchor:footerView.bottomAnchor constant:-21].active=YES;
     [tracklbl.widthAnchor constraintEqualToAnchor:trackBtn.widthAnchor constant:0].active=YES;
-    [tracklbl.heightAnchor constraintLessThanOrEqualToConstant:21].active=YES;
+    [tracklbl.heightAnchor constraintEqualToConstant:21].active=YES;
     tracklbl.text=@"Track Order";
     tracklbl.font=RalewayRegular(appDelegate.font-7);
     tracklbl.textColor=Singlecolor(grayColor);
@@ -124,7 +125,7 @@
     [dividerView.leftAnchor constraintEqualToAnchor:trackBtn.rightAnchor constant:0].active=YES;
     [dividerView.centerYAnchor constraintEqualToAnchor:footerView.centerYAnchor constant:0].active=YES;
     [dividerView.widthAnchor constraintLessThanOrEqualToConstant:1].active=YES;
-    [dividerView.heightAnchor constraintLessThanOrEqualToConstant:10].active=YES;
+    [dividerView.heightAnchor constraintEqualToConstant:10].active=YES;
     dividerView.backgroundColor=Singlecolor(lightGrayColor);
 
     
@@ -135,25 +136,107 @@
     [addvehicleBtn.widthAnchor constraintEqualToConstant:SCREEN_WIDTH/2].active=YES;
     [addvehicleBtn.heightAnchor constraintEqualToAnchor:footerView.heightAnchor constant:0].active=YES;
     [addvehicleBtn.leftAnchor constraintEqualToAnchor:dividerView.rightAnchor constant:2].active=YES;
-    addvehicleBtn.backgroundColor=Singlecolor(greenColor);
-   // [addvehicleBtn addTarget:self action:@selector(addAction) forControlEvents:UIControlEventTouchUpInside];
+    [addvehicleBtn addTarget:self action:@selector(addAction) forControlEvents:UIControlEventTouchUpInside];
     
     
-    UIImageView * addImg=[[UIImageView alloc]initWithFrame:CGRectMake(addvehicleBtn.frame.size.width/2-10, 7, 20, 20)];
+    UIImageView * addImg=[[UIImageView alloc]init];
+    [addvehicleBtn addSubview:addImg];
+    addImg.translatesAutoresizingMaskIntoConstraints = NO;
+    [addImg.centerXAnchor constraintEqualToAnchor:addvehicleBtn.centerXAnchor constant:0].active=YES;
+    [addImg.topAnchor constraintEqualToAnchor:addvehicleBtn.topAnchor constant:7].active=YES;
+    [addImg.widthAnchor constraintLessThanOrEqualToConstant:20].active=YES;
+    [addImg.heightAnchor constraintEqualToConstant:20].active=YES;
     addImg.image=image(@"addvechicle");
     [addvehicleBtn addSubview:addImg];
     
-    UILabel * addLbl=[[UILabel alloc]initWithFrame:CGRectMake(0, footerView.frame.size.height-21, addvehicleBtn.frame.size.width, 21)];
+    UILabel * addLbl=[[UILabel alloc]init];
+    [addvehicleBtn addSubview:addLbl];
+    addLbl.translatesAutoresizingMaskIntoConstraints = NO;
+    [addLbl.topAnchor constraintEqualToAnchor:footerView.bottomAnchor constant:-21].active=YES;
+    [addLbl.leftAnchor constraintEqualToAnchor:trackBtn.rightAnchor constant:0].active=YES;
+    [addLbl.widthAnchor constraintEqualToAnchor:addvehicleBtn.widthAnchor constant:0].active=YES;
+    [addLbl.heightAnchor constraintEqualToConstant:21].active=YES;
     addLbl.text=@"Add Vehicle";
     addLbl.font=RalewayRegular(appDelegate.font-7);
     addLbl.textColor=Singlecolor(grayColor);
     addLbl.textAlignment=NSTextAlignmentCenter;
-    [addvehicleBtn addSubview:addLbl];
+    
+    onrdBtn=[[UIButton alloc]init];
+    [self.view addSubview:onrdBtn];
+    onrdBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [onrdBtn.topAnchor constraintEqualToAnchor:pageControl.bottomAnchor constant:10].active=YES;
+    [onrdBtn.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:30].active=YES;
+    [onrdBtn.widthAnchor constraintEqualToConstant:SCREEN_WIDTH/2.7].active=YES;
+    [onrdBtn.heightAnchor constraintEqualToConstant:30].active=YES;
+    onrdBtn.backgroundColor=RGB(0, 88, 42);
+    [onrdBtn setTitle:@"On Road Service" forState:UIControlStateNormal];
+    onrdBtn.titleLabel.font=RalewayBold(appDelegate.font-2);
+    [onrdBtn setTitleColor:Singlecolor(whiteColor) forState:UIControlStateNormal];
+    onrdBtn.tag=0;
+    [onrdBtn addTarget:self action:@selector(serviceAction:) forControlEvents:UIControlEventTouchUpInside];
+    onrdBtn.layer.cornerRadius = 10;
+    onrdBtn.layer.masksToBounds = YES;
     
     
     
+    prebkBtn=[[UIButton alloc]init];
+    [self.view addSubview:prebkBtn];
+    prebkBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [prebkBtn.topAnchor constraintEqualToAnchor:pageControl.bottomAnchor constant:10].active=YES;
+    [prebkBtn.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:-30].active=YES;
+    [prebkBtn.widthAnchor constraintEqualToConstant:SCREEN_WIDTH/2.7].active=YES;
+    [prebkBtn.heightAnchor constraintEqualToConstant:30].active=YES;
+    [prebkBtn setTitle:@"Pre Booking" forState:UIControlStateNormal];
+    prebkBtn.titleLabel.font=RalewayBold(appDelegate.font-2);
+    [prebkBtn setTitleColor:Singlecolor(blackColor) forState:UIControlStateNormal];
+    prebkBtn.backgroundColor=[UIColor clearColor];
+    prebkBtn.tag=1;
+    [prebkBtn addTarget:self action:@selector(serviceAction:) forControlEvents:UIControlEventTouchUpInside];
+    prebkBtn.layer.cornerRadius = 10;
+    prebkBtn.layer.masksToBounds = YES;
+    
+    onrdScrl=[[UIScrollView alloc]init];
+    onrdScrl.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:onrdScrl];
+    [onrdScrl.topAnchor constraintEqualToAnchor:onrdBtn.bottomAnchor constant:10].active=YES;
+    [onrdScrl.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-50].active=YES;
+    [onrdScrl.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:0].active=true;
+    [onrdScrl.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:0].active=true;
+    onrdScrl.backgroundColor=Singlecolor(greenColor);
+    
+    
+//    prebookScrl=[[UIScrollView alloc]init];
+//    prebookScrl.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self.view addSubview:prebookScrl];
+//    [prebookScrl.topAnchor constraintEqualToAnchor:onrdBtn.bottomAnchor constant:10].active=YES;
+//    [prebookScrl.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-50].active=YES;
+//    [prebookScrl.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:0].active=true;
+//    [prebookScrl.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:0].active=true;
+//    prebookScrl.backgroundColor=Singlecolor(greenColor);
     
 }
+- (void)serviceAction:(id)sender
+{
+    if ([sender tag]==0) {
+        onrdBtn.backgroundColor=RGB(0, 89, 42);
+        [onrdBtn setTitleColor:Singlecolor(whiteColor) forState:UIControlStateNormal];
+        prebkBtn.backgroundColor=Singlecolor(clearColor);
+        [prebkBtn setTitleColor:Singlecolor(blackColor) forState:UIControlStateNormal];
+        onrdScrl.hidden=NO;
+        prebookScrl.hidden=YES;
+    }
+    else
+    {
+        onrdBtn.backgroundColor=Singlecolor(clearColor);
+        [onrdBtn setTitleColor:Singlecolor(blackColor) forState:UIControlStateNormal];
+        prebkBtn.backgroundColor=RGB(0, 89, 42);
+        [prebkBtn setTitleColor:Singlecolor(whiteColor) forState:UIControlStateNormal];
+        onrdScrl.hidden=YES;
+        prebookScrl.hidden=NO;
+    }
+}
+
+
 
 - (void)timerAction
 {
@@ -170,5 +253,9 @@
      [bannerSrl setContentOffset:CGPointMake(xslider, 0)];
     pageControl.currentPage=jslider;
 }
-
+- (void)addAction
+{
+    AddvechicleViewController * addvechicle=[[AddvechicleViewController alloc]init];
+    [self.navigationController pushViewController:addvechicle animated:YES];
+}
 @end
