2b9f2b100c4bb6134bb7ea7d6132406f235e83dd
Arun kumar <k2b.arunkumar@gmail.com>
2019-01-02 14:16:01 +0530

    Jan 2

--- Motoserve/Classes/DraggableView/DraggableViewController.m
+++ Motoserve/Classes/DraggableView/DraggableViewController.m
@@ -13,6 +13,12 @@
 {
     UIScrollView *  statusScroll;
     AppDelegate * appDelegate;
+    UIView * prepareView,* startView,* rchView;
+    NSDateFormatter *dateFormat;
+    UIImageView * prepareImg,* startImg,* rchImg,* doneImg;
+    UILabel *  prepareLbl,* startLbl,* starttimeLbl,* rchLbl,* rchtimeLbl,* doneLbl,* donetimeLbl;
+    UIButton *  doneBtn;
+    int scrollheight;
 }
 @end
 
@@ -21,6 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    dateFormat = [[NSDateFormatter alloc] init];
     [self createDesign];
 }
 
@@ -40,18 +47,9 @@
     
     statusScroll=[[UIScrollView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(dragBtn.frame), swipeView.frame.size.width, swipeView.frame.size.height-CGRectGetMaxY(dragBtn.frame))];
     [swipeView addSubview:statusScroll];
-    statusScroll.contentSize=CGSizeMake(320, 2000);
+    
     //statusScroll.backgroundColor=Singlecolor(redColor);
     
-    [self createSubDesign];
-}
-- (void)gestureHandlerMethod
-{
-   [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-- (void)createSubDesign
-{
     UILabel * vehiclenoLbl=[[UILabel alloc]initWithFrame:CGRectMake(0, 10, statusScroll.frame.size.width, 21)];
     vehiclenoLbl.text=@"TN 45 1420 - Hero Splendor Pro";
     vehiclenoLbl.font=RalewayRegular(appDelegate.font-2);
@@ -74,7 +72,10 @@
     
     
     UILabel * dateLbl=[[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(bookingidLbl.frame), bookingidLbl.frame.origin.y, bookingidLbl.frame.size.width, bookingidLbl.frame.size.height)];
-    dateLbl.text=@"Date:12.01.2019";
+    NSDate * startdate=[NSDate date];
+    [dateFormat setDateFormat:@"dd.mm.yyyy"];
+    dateLbl.text=[NSString stringWithFormat:@"Date:%@",[dateFormat stringFromDate:startdate]];
+    //dateLbl.text=@"Date:12.01.2019";
     dateLbl.font=RalewayRegular(appDelegate.font-2);
     dateLbl.textAlignment=NSTextAlignmentRight;
     [statusScroll addSubview:dateLbl];
@@ -107,24 +108,161 @@
     UILabel * noLbl=[[UILabel alloc]initWithFrame:CGRectMake(nameLbl.frame.origin.x+10, CGRectGetMaxY(divView.frame)+20, nameLbl.frame.size.width/1.8, 21)];
     noLbl.text=@"9787430308";
     noLbl.textAlignment=NSTextAlignmentCenter;
-   // noLbl.backgroundColor=Singlecolor(grayColor);
+    // noLbl.backgroundColor=Singlecolor(grayColor);
     [waitingView addSubview:noLbl];
     
     UIView * div2View=[[UIView alloc]initWithFrame:CGRectMake(CGRectGetMaxX(noLbl.frame), noLbl.frame.origin.y,1, 21)];
     div2View.backgroundColor=Singlecolor(lightGrayColor);
     [waitingView addSubview:div2View];
     
+    UIImageView * callImg=[[UIImageView alloc]initWithFrame:CGRectMake(CGRectGetMaxX(div2View.frame)+10, CGRectGetMaxY(divView.frame)+10, waitingView.frame.size.width/8, waitingView.frame.size.width/8)];
+    callImg.layer.cornerRadius = callImg.frame.size.width/2;
+    callImg.layer.masksToBounds = YES;
+    callImg.image=image(@"logo");
+    [waitingView addSubview:callImg];
+    
     UILabel * statusLbl=[[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(waitingView.frame)+10, statusScroll.frame.size.width, 21)];
     statusLbl.text=@"Status";
     statusLbl.textAlignment=NSTextAlignmentCenter;
     statusLbl.font=RalewayRegular(appDelegate.font-2);
     [statusScroll addSubview:statusLbl];
-    [statusLbl autowidth:0.0];
     
     
+    prepareImg=[[UIImageView alloc]initWithFrame:CGRectMake(waitingView.frame.origin.x+40, CGRectGetMaxY(statusLbl.frame)+30, 11, 10)];
+    prepareImg.image=image(@"Progress");
+    [statusScroll addSubview:prepareImg];
     
+    prepareLbl=[[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(prepareImg.frame)+20,CGRectGetMaxY(statusLbl.frame)+25,statusLbl.frame.size.width-120, 21)];
+    prepareLbl.text=@"Preparing Tools";
+    prepareLbl.textColor=Singlecolor(blackColor);
     
+    prepareLbl.font=RalewayRegular(appDelegate.font-2);
+    [statusScroll addSubview:prepareLbl];
     
+    UILabel * preparetimeLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x, CGRectGetMaxY(prepareLbl.frame), prepareLbl.frame.size.width, 21)];
+    NSDate * date=[NSDate date];
+    [dateFormat setDateFormat:@"hh:mm a"];
+    preparetimeLbl.text=[dateFormat stringFromDate:date];
+    preparetimeLbl.textColor=Singlecolor(lightGrayColor);
+    preparetimeLbl.font=RalewayRegular(appDelegate.font-4);
+    [statusScroll addSubview:preparetimeLbl];
+    
+    scrollheight=CGRectGetMaxY(preparetimeLbl.frame);
+    
+    [self receivesegmentNotification];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(receivesegmentNotification)
+                                                 name:@"changetype"
+                                               object:nil];
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+}
+- (void)gestureHandlerMethod
+{
+   [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)receivesegmentNotification
+{
+    
+    if ([appDelegate.bookingstatusStr isEqualToString:@"2"]) {
+        prepareView=[[UIView alloc]initWithFrame:CGRectMake(prepareImg.frame.origin.x+5, CGRectGetMaxY(prepareImg.frame), 1, 80)];
+        prepareView.backgroundColor=RGB(0, 90, 45);
+        [statusScroll addSubview:prepareView];
+        
+       startImg=[[UIImageView alloc]initWithFrame:CGRectMake(prepareImg.frame.origin.x, CGRectGetMaxY(prepareView.frame), 11, 10)];
+        startImg.image=image(@"Progress");
+        [statusScroll addSubview:startImg];
+        
+        startLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x,startImg.frame.origin.y-5,prepareLbl.frame.size.width, 21)];
+        startLbl.text=@"Start Navigation";
+        startLbl.textColor=Singlecolor(blackColor);
+        startLbl.font=RalewayRegular(appDelegate.font-2);
+        [statusScroll addSubview:startLbl];
+        
+        
+        starttimeLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x, CGRectGetMaxY(startLbl.frame), prepareLbl.frame.size.width, 21)];
+        NSDate * startdate=[NSDate date];
+        [dateFormat setDateFormat:@"hh:mm a"];
+        starttimeLbl.text=[dateFormat stringFromDate:startdate];
+        starttimeLbl.textColor=Singlecolor(lightGrayColor);
+        starttimeLbl.font=RalewayRegular(appDelegate.font-4);
+        [statusScroll addSubview:starttimeLbl];
+        
+        scrollheight=CGRectGetMaxY(starttimeLbl.frame);
+    }
+    else if ([appDelegate.bookingstatusStr isEqualToString:@"3"])
+    {
+    startView=[[UIView alloc]initWithFrame:CGRectMake(prepareView.frame.origin.x, CGRectGetMaxY(startImg.frame), 1, prepareView.frame.size.height)];
+        startView.backgroundColor=RGB(0, 90, 45);
+        [statusScroll addSubview:startView];
+        
+        
+    rchImg=[[UIImageView alloc]initWithFrame:CGRectMake(prepareImg.frame.origin.x, CGRectGetMaxY(startView.frame), 11, 10)];
+        rchImg.image=image(@"Progress");
+        [statusScroll addSubview:rchImg];
+        
+    rchLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x,rchImg.frame.origin.y-5,prepareLbl.frame.size.width, 21)];
+        rchLbl.text=@"Work In Progress";
+        rchLbl.textColor=Singlecolor(blackColor);
+        rchLbl.font=RalewayRegular(appDelegate.font-2);
+        [statusScroll addSubview:rchLbl];
+        
+    rchtimeLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x, CGRectGetMaxY(rchLbl.frame), prepareLbl.frame.size.width, 21)];
+        NSDate * rchdate=[NSDate date];
+        [dateFormat setDateFormat:@"hh:mm a"];
+        rchtimeLbl.text=[dateFormat stringFromDate:rchdate];
+        rchtimeLbl.textColor=Singlecolor(lightGrayColor);
+        rchtimeLbl.font=RalewayRegular(appDelegate.font-4);
+        [statusScroll addSubview:rchtimeLbl];
+        
+        scrollheight=CGRectGetMaxY(rchtimeLbl.frame);
+    }
+    else if ([appDelegate.bookingstatusStr isEqualToString:@"4"])
+    {
+    rchView=[[UIView alloc]initWithFrame:CGRectMake(prepareView.frame.origin.x, CGRectGetMaxY(rchImg.frame), 1, prepareView.frame.size.height)];
+        rchView.backgroundColor=RGB(0, 90, 45);
+        [statusScroll addSubview:rchView];
+        
+        
+    doneImg=[[UIImageView alloc]initWithFrame:CGRectMake(prepareImg.frame.origin.x, CGRectGetMaxY(rchView.frame), 11, 10)];
+        doneImg.image=image(@"Progress");
+        [statusScroll addSubview:doneImg];
+        
+    doneLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x,doneImg.frame.origin.y-5,prepareLbl.frame.size.width, 21)];
+        doneLbl.text=@"Work Done";
+        doneLbl.textColor=Singlecolor(blackColor);
+        doneLbl.font=RalewayRegular(self->appDelegate.font-2);
+        [self->statusScroll addSubview:doneLbl];
+        
+    donetimeLbl=[[UILabel alloc]initWithFrame:CGRectMake(prepareLbl.frame.origin.x, CGRectGetMaxY(doneLbl.frame), prepareLbl.frame.size.width, 21)];
+        NSDate * donedate=[NSDate date];
+        [dateFormat setDateFormat:@"hh:mm a"];
+        donetimeLbl.text=[dateFormat stringFromDate:donedate];
+        donetimeLbl.textColor=Singlecolor(lightGrayColor);
+        donetimeLbl.font=RalewayRegular(appDelegate.font-4);
+        [statusScroll addSubview:donetimeLbl];
+        
+        
+    doneBtn=[[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH/2.0-50, CGRectGetMaxY(doneLbl.frame)+40, 100, 30)];
+        [doneBtn setBackgroundColor:Singlecolor(clearColor)];
+        [doneBtn setTitle:@"Done" forState:UIControlStateNormal];
+        doneBtn.titleLabel.font=RalewayRegular(self->appDelegate.font-2);
+        [doneBtn setTitleColor:RGB(0, 90, 45) forState:UIControlStateNormal];
+        doneBtn.layer.cornerRadius = 5;
+        doneBtn.layer.borderWidth = 0.5;
+        doneBtn.layer.masksToBounds = true;
+        doneBtn.layer.borderColor = [Singlecolor(lightGrayColor) CGColor];
+        //[doneBtn addTarget:self action:@selector(doneAction) forControlEvents:UIControlEventTouchUpInside];
+        [statusScroll addSubview:doneBtn];
+        
+        scrollheight=CGRectGetMaxY(doneBtn.frame);
+    }
+    
+    statusScroll.contentSize=CGSizeMake(320, scrollheight+20);
 }
 
 @end
--- Motoserve/Classes/Mapview/MapViewController.m
+++ Motoserve/Classes/Mapview/MapViewController.m
@@ -25,10 +25,8 @@
     AppDelegate * appDelegate;
     NSString * bookstatusStr;
     NSTimer * getbookingtimer,*patnerlocationtimer;
-    UIScrollView * statusScroll;
-    UIImageView * prepareImg,* startImg,* rchImg;
-    UILabel * prepareLbl,* startLbl,* rchLbl,* doneLbl;
     CLLocationManager * locationManager;
+    DraggableViewController * ordernw;
 }
 
 @property (strong, nonatomic) ARCarMovement *moveMent;
@@ -105,55 +103,7 @@ UIAlertView * alert=[[UIAlertView alloc]initWithTitle:[[[NSBundle mainBundle] in
     dragBtn.layer.borderColor = [UIColor blackColor].CGColor;
     [self.view addSubview:dragBtn];
     
-    
-//    UIView * statusview=[[UIView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(mapView.frame), SCREEN_WIDTH, mapView.frame.size.height)];
-//    statusview.backgroundColor=Singlecolor(whiteColor);
-//    [self.view addSubview:statusview];
-//
-//    statusScroll=[[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, statusview.frame.size.width, statusview.frame.size.height)];
-//    //statusScroll.backgroundColor=Singlecolor(blackColor);
-//    [statusview addSubview:statusScroll];
-//
-//    UIButton *  submitBtn=[[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH/2.0-75, 10, 150, 30)];
-//    [submitBtn setBackgroundColor:Singlecolor(clearColor)];
-//    [submitBtn setTitle:@"Request Accepted" forState:UIControlStateNormal];
-//    submitBtn.titleLabel.font=RalewayRegular(appDelegate.font-2);
-//    [submitBtn setTitleColor:RGB(113, 209, 154) forState:UIControlStateNormal];
-//    submitBtn.layer.cornerRadius = 5;
-//    submitBtn.layer.borderWidth = 0.5;
-//    submitBtn.layer.masksToBounds = true;
-//    submitBtn.layer.borderColor = [Singlecolor(lightGrayColor) CGColor];
-//    //[submitBtn addTarget:self action:@selector(submitAction) forControlEvents:UIControlEventTouchUpInside];
-//    [statusScroll addSubview:submitBtn];
-//
-//    UILabel * providernameLbl=[[UILabel alloc]initWithFrame:CGRectMake(20,CGRectGetMaxY(submitBtn.frame)+20,statusview.frame.size.width-40, 21)];
-//    providernameLbl.text=[[NSString stringWithFormat:@"Service Provider Name  :   %@",self.serviceprovidername]capitalizedString];
-//    providernameLbl.textColor=Singlecolor(blackColor);
-//    providernameLbl.font=RalewayBold(appDelegate.font-2);
-//    [statusScroll addSubview:providernameLbl];
-//
-//    UILabel * distanceLbl=[[UILabel alloc]initWithFrame:CGRectMake(20,CGRectGetMaxY(providernameLbl.frame)+5,statusview.frame.size.width-40, 21)];
-//    distanceLbl.text=@"Distance                :     2KM";
-//    distanceLbl.textColor=Singlecolor(blackColor);
-//    distanceLbl.font=RalewayRegular(appDelegate.font-2);
-//    [statusScroll addSubview:distanceLbl];
-//
-//    UILabel * estimateLbl=[[UILabel alloc]initWithFrame:CGRectMake(20,CGRectGetMaxY(distanceLbl.frame)+5,statusview.frame.size.width-40, 21)];
-//    estimateLbl.text=@"Estimated Time    :    10Mins";
-//    estimateLbl.textColor=Singlecolor(blackColor);
-//    estimateLbl.font=RalewayRegular(appDelegate.font-2);
-//    [statusScroll addSubview:estimateLbl];
-//
-//    prepareImg=[[UIImageView alloc]initWithFrame:CGRectMake(40, CGRectGetMaxY(estimateLbl.frame)+30, 11, 10)];
-//    prepareImg.image=image(@"Progress");
-//    [statusScroll addSubview:prepareImg];
-//
-//    prepareLbl=[[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(prepareImg.frame)+20,CGRectGetMaxY(estimateLbl.frame)+25,statusview.frame.size.width-100, 21)];
-//    prepareLbl.text=@"Preparing Tools";
-//    prepareLbl.textColor=Singlecolor(blackColor);
-//    //prepareLbl.backgroundColor=Singlecolor(grayColor);
-//    prepareLbl.font=RalewayRegular(appDelegate.font-2);
-//    [statusScroll addSubview:prepareLbl];
+    ordernw = [[DraggableViewController alloc]init];
     
     //=======
     
@@ -171,11 +121,36 @@ UIAlertView * alert=[[UIAlertView alloc]initWithTitle:[[[NSBundle mainBundle] in
     //start the timer, change the interval based on your requirement
     //
    // getbookingtimer=[NSTimer scheduledTimerWithTimeInterval:10.0 target:self selector:@selector(getbooking) userInfo:nil repeats:YES];
+    
+    appDelegate.bookingstatusStr=@"2";
+    [NSTimer scheduledTimerWithTimeInterval:10.0
+                                     target:self
+                                   selector:@selector(targetMethod)
+                                   userInfo:nil
+                                    repeats:NO];
 }
 
+- (void)targetMethod
+{
+    appDelegate.bookingstatusStr=@"3";
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:@"changetype"
+     object:nil];
+    [NSTimer scheduledTimerWithTimeInterval:10.0
+                                     target:self
+                                   selector:@selector(targetMethod1)
+                                   userInfo:nil
+                                    repeats:NO];
+}
+- (void)targetMethod1
+{
+    appDelegate.bookingstatusStr=@"4";
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:@"changetype"
+     object:nil];
+}
 - (void)gestureHandlerMethod
 {
-    DraggableViewController * ordernw = [[DraggableViewController alloc]init];
     ordernw.view.backgroundColor = [UIColor clearColor];
     self.modalPresentationStyle = UIModalPresentationCurrentContext;
 #ifdef __IPHONE_8_0
@@ -208,6 +183,12 @@ UIAlertView * alert=[[UIAlertView alloc]initWithTitle:[[[NSBundle mainBundle] in
          {
              NSLog(@"1");
              
+             if (![self->appDelegate.bookingstatusStr isEqualToString:[[[responseObject valueForKey:@"data"]valueForKey:@"booking"]valueForKey:@"bookingStatus"]]) {
+                 [[NSNotificationCenter defaultCenter]
+                      postNotificationName:@"changetype"
+                      object:nil];
+             }
+             
              if ([self->appDelegate.bookingstatusStr isEqualToString:@"2"]) {
                  return ;
              }
@@ -217,100 +198,20 @@ UIAlertView * alert=[[UIAlertView alloc]initWithTitle:[[[NSBundle mainBundle] in
                 
                  self->patnerlocationtimer = [NSTimer scheduledTimerWithTimeInterval:15.0 target:self selector:@selector(getpartnerlocation) userInfo:nil repeats:true];
                 
-                 
-//                 self->prepareView=[[UIView alloc]initWithFrame:CGRectMake(self->prepareImg.frame.origin.x+5, CGRectGetMaxY(self->prepareImg.frame), 1, 40)];
-//                 self->prepareView.backgroundColor=RGB(95, 208, 154);
-//                 [self->statusScroll addSubview:self->prepareView];
-//                 
-//                 self->startImg=[[UIImageView alloc]initWithFrame:CGRectMake(40, CGRectGetMaxY(self->prepareImg.frame)+40, 11, 10)];
-//                 self->startImg.image=image(@"Progress");
-//                 [self->statusScroll addSubview:self->startImg];
-//                 
-//                 self->startLbl=[[UILabel alloc]initWithFrame:CGRectMake(self->prepareLbl.frame.origin.x,CGRectGetMaxY(self->prepareLbl.frame)+30,self->prepareLbl.frame.size.width, 21)];
-//                 self->startLbl.text=@"Start Navigation";
-//                 self->startLbl.textColor=Singlecolor(blackColor);
-//                 self->startLbl.font=RalewayRegular(self->appDelegate.font-2);
-//                 [self->statusScroll addSubview:self->startLbl];
-                 
              }
              else
              {
                  self->appDelegate.bookingstatusStr=[[[responseObject valueForKey:@"data"]valueForKey:@"booking"]valueForKey:@"bookingStatus"];
              }
-             
-             
-             
-             
-//             else if ([[[[responseObject valueForKey:@"data"]valueForKey:@"booking"]valueForKey:@"bookingStatus"]isEqualToString:@"3"])
-//             {
-//                 self->appDelegate.bookingstatusStr=@"3";
-//                 [self->patnerlocationtimer invalidate];
-//                 self->patnerlocationtimer = nil;
-//
-//                 self->startView=[[UIView alloc]initWithFrame:CGRectMake(self->prepareView.frame.origin.x, CGRectGetMaxY(self->startImg.frame), 1, 40)];
-//                 self->startView.backgroundColor=RGB(95, 208, 154);
-//                 [self->statusScroll addSubview:self->startView];
-//
-//
-//                 self->rchImg=[[UIImageView alloc]initWithFrame:CGRectMake(40, CGRectGetMaxY(self->startImg.frame)+40, 11, 10)];
-//                 self->rchImg.image=image(@"Progress");
-//                 [self->statusScroll addSubview:self->rchImg];
-//
-//                 self->rchLbl=[[UILabel alloc]initWithFrame:CGRectMake(self->prepareLbl.frame.origin.x,CGRectGetMaxY(self->startLbl.frame)+30,self->prepareLbl.frame.size.width, 21)];
-//                 self->rchLbl.text=@"Work In Progress";
-//                 self->rchLbl.textColor=Singlecolor(blackColor);
-//                 self->rchLbl.font=RalewayRegular(self->appDelegate.font-2);
-//                 [self->statusScroll addSubview:self->rchLbl];
-//
-//
-//
-//             }
-//             else if ([[[[responseObject valueForKey:@"data"]valueForKey:@"booking"]valueForKey:@"bookingStatus"]isEqualToString:@"4"])
-//             {
-//                 self->appDelegate.bookingstatusStr=@"4";
-//
-//                 UIView * rchView=[[UIView alloc]initWithFrame:CGRectMake(self->prepareView.frame.origin.x, CGRectGetMaxY(self->rchImg.frame), 1, 40)];
-//                 rchView.backgroundColor=RGB(95, 208, 154);
-//                 [self->statusScroll addSubview:rchView];
-//
-//
-//                 UIImageView * doneImg=[[UIImageView alloc]initWithFrame:CGRectMake(40, CGRectGetMaxY(self->rchImg.frame)+40, 11, 10)];
-//                 doneImg.image=image(@"Progress");
-//                 [self->statusScroll addSubview:doneImg];
-//
-//                 self->doneLbl=[[UILabel alloc]initWithFrame:CGRectMake(self->prepareLbl.frame.origin.x,CGRectGetMaxY(self->rchLbl.frame)+30,self->prepareLbl.frame.size.width, 21)];
-//                 self->doneLbl.text=@"Work Done";
-//                 self->doneLbl.textColor=Singlecolor(blackColor);
-//                 self->doneLbl.font=RalewayRegular(self->appDelegate.font-2);
-//                 [self->statusScroll addSubview:self->doneLbl];
-//
-//
-//                 UIButton *  doneBtn=[[UIButton alloc]initWithFrame:CGRectMake(SCREEN_WIDTH/2.0-50, CGRectGetMaxY(self->doneLbl.frame)+10, 100, 30)];
-//                 [doneBtn setBackgroundColor:Singlecolor(clearColor)];
-//                 [doneBtn setTitle:@"Completed" forState:UIControlStateNormal];
-//                 doneBtn.titleLabel.font=RalewayRegular(self->appDelegate.font-2);
-//                 [doneBtn setTitleColor:RGB(113, 209, 154) forState:UIControlStateNormal];
-//                 doneBtn.layer.cornerRadius = 5;
-//                 doneBtn.layer.borderWidth = 0.5;
-//                 doneBtn.layer.masksToBounds = true;
-//                 doneBtn.layer.borderColor = [Singlecolor(lightGrayColor) CGColor];
-//                 [doneBtn addTarget:self action:@selector(doneAction) forControlEvents:UIControlEventTouchUpInside];
-//                 [self->statusScroll addSubview:doneBtn];
-//
-//
-//             }
-//             else if ([[[[responseObject valueForKey:@"data"]valueForKey:@"booking"]valueForKey:@"bookingStatus"]isEqualToString:@"6"])
-//             {
-//                 self->appDelegate.bookingstatusStr=@"6";
-//
-//                 [self->getbookingtimer invalidate];
-//                 self->getbookingtimer = nil;
-//                 billViewController * bill=[[billViewController alloc]init];
-//                 bill.bookidStr=self->appDelegate.bookingidStr;
-//                 [self.navigationController pushViewController:bill animated:YES];
-//             }
+             if ([self->appDelegate.bookingstatusStr isEqualToString:@"6"])
+             {
+                 [self->getbookingtimer invalidate];
+                 self->getbookingtimer = nil;
+                 billViewController * bill=[[billViewController alloc]init];
+                 bill.bookidStr=self->appDelegate.bookingidStr;
+                 [self.navigationController pushViewController:bill animated:YES];
+             }
          }
-         //self->statusScroll.contentSize=CGSizeMake(SCREEN_WIDTH, self.view.frame.size.height/1.5);
      } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
          NSLog(@"Error: %@", error);
          [Utils showErrorAlert:@"Check Your Inertnet Connection" delegate:nil];
@@ -399,7 +300,7 @@ UIAlertView * alert=[[UIAlertView alloc]initWithTitle:[[[NSBundle mainBundle] in
 
 -(void)viewWillAppear:(BOOL)animated
 {
-    
+    [self gestureHandlerMethod];
 }
 #pragma mark - scheduledTimerWithTimeInterval Action
 -(void)timerTriggered {
